936aadd4ba8e5d99e4b2bc0158ba60aa6d265e5f
musicjoe12 <josephgrierson8@gmail.com>
2022-11-11 10:47:29 +0000

    colours

--- js/main.js
+++ js/main.js
@@ -2,7 +2,21 @@
     document.getElementById("myHeading").innerHTML ="Joe"
     document.querySelector("nav ul li").setAttribute("class", "currentPage")
 
-    document.getElementById("myTestEvent").onClick = function(){
-        console.info("Hi")
-    }
+    document.getElementById("myTestEvent").addEventListener("click", function(){
+        console.info("I was clicked")
+    })
+
+    document.querySelector(".red").addEventListener('click', function(ev){
+        document.querySelector('body').setAttribute('class', "redBack");
+    })
+       
+    document.querySelector(".green").addEventListener('click', function(ev){
+            document.querySelector('body').setAttribute('class', "greenBack");
+    })
+    document.querySelector(".blue").addEventListener('click', function(ev){
+        document.querySelector('body').setAttribute('class', "blueBack");
+    })
+    document.querySelector(".default").addEventListener('click', function(ev){
+        document.querySelector('body').removeAttribute('class');
+    })
 })()
